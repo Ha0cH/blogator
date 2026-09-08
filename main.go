e96c40f143a1e8d4e19415564abeb21e571fd23c
Hao Chen <hucklol116@gmail.com>
2026-09-08 16:58:13 -0600

    Add database/sql

--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/Ha0cH/blogator/internal/config"
+	"github.com/Ha0cH/blogator/internal/database"
+	_ "github.com/lib/pq"
 )
 
 type state struct {
 	cfg *config.Config
+	db  *database.Queries
 }
 
 func main() {
@@ -18,8 +22,17 @@ func main() {
 		log.Fatalf("Error reading config: %v\n", err)
 	}
 
+	db, err := sql.Open("postgres", cfg.DBURL)
+	if err != nil {
+		log.Fatalf("Error connecting to database: %v\n", err)
+	}
+	defer db.Close()
+
+	dbQueries := database.New(db)
+
 	var s state
 	s.cfg = &cfg
+	s.db = dbQueries
 
 	var cmds commands
 	cmds.cmdLookUp = make(map[string]func(*state, command) error)
