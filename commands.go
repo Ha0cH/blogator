3649a76f49ce34fc16a6c7ae53c10e3843808a46
Hao Chen <hucklol116@gmail.com>
2026-09-20 15:16:27 -0600

    Add rss_feed structure and agg command

--- commands.go
+++ commands.go
@@ -104,3 +104,12 @@ func handlerUsers(s *state, cmd command) error {
 	}
 	return nil
 }
+
+func handlerAgg(s *state, cmd command) error {
+	f, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Fetched feed: %+v\n", f)
+	return nil
+}
--- main.go
+++ main.go
@@ -41,6 +41,7 @@ func main() {
 	cmds.register("register", handlerRegister)
 	cmds.register("reset", handlerReset)
 	cmds.register("users", handlerUsers)
+	cmds.register("agg", handlerAgg)
 
 	if len(os.Args) < 2 {
 		fmt.Println("Program requires at least a command, and its arguments if needed.")
